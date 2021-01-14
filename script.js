d471e41cfe39063416029bc83e73ffa5bd1edd6a
baturaillia <75522734+baturaillia@users.noreply.github.com>
2021-01-14 19:33:23 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+//let fname = prompt('вкажи своє ім\'я');
+//let sname = prompt('вкажи своє прізвище');
+//let password = prompt('придумай свій пароль');
+//console.log(`привіт ${fname} ${sname}, твій пароль ${password}`);
